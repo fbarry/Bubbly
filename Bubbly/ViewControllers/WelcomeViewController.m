5be28f1496b229eb4853ddb86824fa4a8337db80
Fiona Barry <fbarry@fb.com>
2020-07-28 11:35:12 -0400

    Fixing

--- Bubbly/ViewControllers/WelcomeViewController.m
+++ Bubbly/ViewControllers/WelcomeViewController.m
@@ -6,16 +6,21 @@
 //  Copyright © 2020 fbarry. All rights reserved.
 //
 
-#import "WelcomeViewController.h"
+#import "LoginViewController.h"
+#import "Utilities.h"
+#import "User.h"
 
-@interface WelcomeViewController ()
+@interface LoginViewController ()
 
 @property (weak, nonatomic) IBOutlet UIButton *login;
 @property (weak, nonatomic) IBOutlet UIButton *signup;
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
 @end
 
-@implementation WelcomeViewController
+@implementation LoginViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -24,12 +29,32 @@
     self.signup.layer.cornerRadius = 16;
 }
 
-- (IBAction)didTapLogin:(id)sender {
-    [self performSegueWithIdentifier:@"Login" sender:self];
+- (IBAction)didTapLogin:(UIButton *)sender {
+    sender.userInteractionEnabled = NO;
+    [self.activityIndicator startAnimating];
+    
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+    
+    [User logInWithUsernameInBackground:username password:password block:^(PFUser * _Nullable user, NSError * _Nullable error) {
+        if (error) {
+            [Utilities presentOkAlertControllerInViewController:self
+                                                      withTitle:@"Error Logging In"
+                                                        message:[NSString stringWithFormat:@"%@", error.localizedDescription]];
+        } else {
+            [self performSegueWithIdentifier:@"Home" sender:self];
+        }
+        sender.userInteractionEnabled = YES;
+        [self.activityIndicator stopAnimating];
+    }];
 }
 
 - (IBAction)didTapSignUp:(id)sender {
     [self performSegueWithIdentifier:@"Signup" sender:self];
+}
+
+- (IBAction)didTapBackground:(id)sender {
+    [self.view endEditing:YES];
 }
 
 #pragma mark - Navigation
